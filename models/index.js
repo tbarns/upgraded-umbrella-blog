3c696c9248827b18ea5e29a9582c08bfd9ec7349
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-14 18:55:10 -0600

    made comments to psuedo code the process

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// the associations are going be between post that eh use creates, the comments on the posts, and the post belongs to user AND the comment belongs to a user 
+
+//seeds will contain users comments and posts
+
+//
---
+++ server.js
@@ -0,0 +1,14 @@
+// copy and paste to create a connection to test routes
+
+
+//schema first
+//models
+//server.js with sequelize sync
+//test to make sure it will run
+//seeds cuz now it will take information
+
+
+//routes
+
+
+//final step is front end
