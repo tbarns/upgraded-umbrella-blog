9c10627da9f1dcb1459d27ca108ff1f7483e450d
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-15 22:33:26 -0600

    working on routing and paths to getcontroller working

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const userRoutes = require('./api/user-routes.js');
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/api/users', userRoutes);
 
 module.exports = router;
