799fa28cf450e3d7d3749ca81405381ae56b4c90
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-15 22:06:39 -0600

    updated seeds

--- models/Comment.js
+++ models/Comment.js
@@ -9,7 +9,26 @@ Comment.init(
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
-        }
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
     },
     {
         sequelize
---
+++ seeds/commentData.js
@@ -0,0 +1,33 @@
+const { Comments } = require('../models');
+
+const commentData = [
+    {
+        body: 'This isnt funny.  Was it meant as a joke?',
+       user_id: 1,
+        post_id: 2,
+    },
+    {
+        body: 'WAIT WHAT?',
+       user_id: 3,
+        post_id: 1,
+    },
+    {
+        body: 'Thats bizzare!',
+       user_id: 2,
+        post_id: 2,
+    },
+    {
+        body: 'Is this true?',
+       user_id: 3,
+        post_id: 3,
+    },
+    {
+        body: 'Thanks for sharing.',
+       user_id: 3,
+        post_id: 2,
+    },
+
+]
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
