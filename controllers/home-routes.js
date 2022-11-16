1a342f733a3c38eafe1265be0f050ac6e82cb83c
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-16 14:57:41 -0600

    finsihed models and seeded DB.  started working on routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const { Comment, Post, User } = require('../models');
 
+//GET all from homepage
+router.get('/', async (req, res) => {
 
+    try {
+        const socialData = await Post.findAll({
+            include: [{
+                model: User
+            }]
+        })
+        const socialPost = socialData.map((post) =>
+            post.get({ plain: true }));
+res.json(socialPost)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+});
 
 
 
